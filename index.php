59d5c51b7ab317f95883a40f2b8971f1c57c6465
JoseJuan <josejuan.ripoll.tous@gmail.com>
2017-02-06 20:23:56 +0100

    Inicio del proyecto

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Despliegue de Aplicaciones LAMP GitHub</title>
+</head>
+<body>
+<h1>Despliegue de aplicaciones LAMP GitHub</h1>
+	<?php
+		phpinfo();
+	?>
+</body>
+</html>
